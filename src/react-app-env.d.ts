5c4d9e651a4ee03b95bb076571384985151fc934
ᴢєɴσꭙ <111524889+phoenix-monarch@users.noreply.github.com>
2023-11-30 10:50:01 +0530

    Update react-app-env.d.ts

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,58 @@
-/// <reference types="react-scripts" />
+{
+  "compilerOptions": {
+    "target": "es6",
+    "esModuleInterop": true,
+    "importHelpers": true,
+    "allowSyntheticDefaultImports": true,
+    "experimentalDecorators": true,
+    "lib": [
+      "dom",
+      "dom.iterable",
+      "esnext",
+      "es5",
+      "es6"
+    ],
+    "baseUrl": ".", // setting a value for baseUrl is required
+    "paths": {
+      "@/*": [
+        "*"
+      ],
+      "~/*": [
+        "*"
+      ]
+    },
+    "allowJs": true,
+    "skipLibCheck": true,
+    "strict": true,
+    "forceConsistentCasingInFileNames": true,
+    "noEmit": true,
+    "module": "esnext",
+    "moduleResolution": "node",
+    "resolveJsonModule": true,
+    "isolatedModules": true,
+    "jsx": "preserve",
+    "declaration": true,
+    "downlevelIteration": true,
+    "noUnusedParameters": false,
+    "noUnusedLocals": false,
+    "incremental": true
+  },
+  "exclude": [
+    "node_modules",
+    ".next",
+    ".vscode",
+    "out"
+  ],
+  "include": [
+    "**/styles/**/*",
+    "**/*.ts",
+    "**/*.tsx",
+    "**/*.yml",
+    "**/*.yaml",
+    "**/*.css",
+    "**/*.scss",
+    "**/*",
+    ".d.ts",
+    "next-env.d.ts",
+  ]
+}
